44a358134fe147a6b88b6c559aa0320b7b1fa831
Hyunmin Kim <oannes@Hyunmins-MacBook-Pro.local>
2016-10-05 00:39:59 -0600

    search interval

--- src/interval.sh
+++ src/interval.sh
@@ -6,52 +6,64 @@
 interval.tree(){
 cat $1 | perl -e 'use strict; 
 	sub add{
-		my ($i,$a,$b,$min, $max, $x)= @_;
-		if( !defined $x){
-			return;
-		}
-		if ( !defined $x->{s} ){ ## initiate this tree
-			$x->{s}=$b;
-			$x->{l}->{s}=$a;
-			$x->{l}->{p}=$x;
-			my %n=();
-			$x->{r}= \%n; 
-			return;
-		}
-		if ( $a <= $min && $max <= $b ){
-			$x->{"i"}=$i;
+		my ($i,$a,$b,$x)= @_;
+		if ( !defined $x->{a} ){ ## insert this node 
+			$x->{a}=$a;
+			$x->{b}=$b;
+			$x->{max}=$b;
+			$x->{l}={};
+			$x->{r}={};
+			return $b;
 		}else{
-			if( $a < $x->{s} ){
-				#print "add $a from $x->{s}\n";
-				add($i,$a,$b, $min,$x->{s},$x->{l});
+			my $max=0;
+			if( $a < $x->{a} ){
+				$max=add($i,$a,$b,$x->{l});
 			}else{
-				add($i,$a,$b, $x->{s},$max,$x->{r});
+				$max=add($i,$a,$b,$x->{r});
 			}
-			if( $b < $x->{s} ){
-				add($i,$a,$b, $min,$x->{s},$x->{l});
-			}else{
-				add($i,$a,$b, $x->{s},$max,$x->{r});
+			if($max > $x->{max}){
+				$x->{max}=$max;
 			}
+			return $x->{max};
 		}
 
 	}
 	sub trav{
 		my ($T,$d) =@_;		
-		if( ! defined $T){ return;}
-		print " "x$d,$T->{s};
-		print ".l:";
-		trav($T->{l},$d++);
-		print ".r;";
-		trav($T->{r},$d++); 
-		print "\n";
+		if( ! defined $T->{a}){ return;}
+		print "-"x$d,join(",",($T->{a},$T->{b},$T->{max})),"\n";
+		trav($T->{l},$d+1);
+		trav($T->{r},$d+1);
+	}
+	sub intersect{
+		my ($a,$b,$x,$y)=@_;
+		if( $b <= $x || $y <= $a){ return 0;}
+		return 1;
+	}
+	sub search{
+		my ($T,$a,$b,$d, $res) =@_;		
+		if( ! defined $T->{a}){ return "";}
+		if( intersect($T->{a},$T->{b},$a,$b) ){ 
+			push @$res,$T->{a}."\t".$T->{b};
+		}
+
+		if(!defined $T->{l}->{max} || $T->{l}->{max} <= $a){
+			search($T->{r},$a,$b,$d+1,$res);
+		}else{
+			search($T->{l},$a,$b,$d+1,$res);
+		}
 	}
 	my %T=();
 	my $i=0; 
 	while(<STDIN>){ chomp; my ($a,$b)=split/\s+/,$_;
-		add($i,$a,$b,"-inf","inf",\%T);
+		add($i,$a,$b,\%T);
 		$i++;
 	}
 	trav(\%T,0);
+	my @res=();
+	search(\%T,24,25,0,\@res);
+	print "found--\n";
+	print join ("\n",@res),"\n";
 
 '
 }
@@ -59,8 +71,9 @@ cat $1 | perl -e 'use strict;
 interval.tree.test(){
 echo \
 "10	20
+15	25
 18	22
-15	25" \
+14	30" \
 | interval.tree -
 
 }
